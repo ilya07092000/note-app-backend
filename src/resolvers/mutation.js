9ca1b3b6a379b7115b957327e967960cc21b6679
ilya07092000 <ischenko2012@gmail.com>
2021-10-12 21:18:14 +0300

    feat: signup/signin mutations

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,11 +1,18 @@
 const models = require('../models/index');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError,
+  ForbiddenError
+} = require('apollo-server-express');
+const gravatar = require('../util/gravatar');
+const crypto = require('crypto');
 
 const Mutation = {
   newNote: async (parent, args, { models }) => {
-    return await models.Note.create ({
+    return await models.Note.create({
       content: args.content,
-      author: 'Me',
-    })
+      author: 'Me'
+    });
   },
   deleteNote: async (parent, { id }, { models }) => {
     try {
@@ -17,19 +24,57 @@ const Mutation = {
     }
   },
   updateNote: async (parent, { content, id }, { models }) => {
-   return await models.Note.findOneAndUpdate(
-    {
-      _id: id
-    },
-    {
-      $set: {
-        content
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
       }
-    },
-    {
-      new: true
+    );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    const hmac = crypto.createHmac('sha512', '10');
+    hmac.update(password);
+    const hashedPass = hmac.digest('hex');
+    const avatar = gravatar(email);
+
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashedPass
+      });
+
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Error with account creating');
     }
-   )
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    const user = await models.User.findOne({
+      $or: [{email}, {username}]
+    })
+
+    if (!user) {
+      throw new AuthenticationError('Error')
+    }
+
+    const hmac = crypto.createHmac('sha512', '10');
+    hmac.update(password);
+    const hashedPass = hmac.digest('hex');
+    
+    if (hashedPass !== user.password) {
+      throw new AuthenticationError('Error');
+    };
+
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   }
 };
 

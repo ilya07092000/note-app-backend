8ae367154a0bf6a85bc317d3ad6e8703e20915d3
ilya07092000 <ischenko2012@gmail.com>
2021-10-14 22:39:07 +0300

    feat: update user queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -5,6 +5,16 @@ const Query = {
   notes: async (parent, args, { models }) => await models.Note.find(),
   note: (parent, args, { models }) => {
     return models.Note.findById(args.id)
+  },
+  user: async (parent, { email }, { models }) => {
+    return await models.User.findOne({ email });
+  },
+  users: async (parent, args, { models }) => {
+    return await models.User.find({});
+  },
+  me: async (parent, args, { model, user }) => {
+    console.log(user);
+    return await models.User.findById(user.id);
   }
 };
 

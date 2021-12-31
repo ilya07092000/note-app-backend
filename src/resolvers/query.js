1e2682795c2dac8b50e474bf43c1b20430a05690
ilya07092000 <ischenko2012@gmail.com>
2021-12-31 12:48:49 +0200

    improve queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,10 +1,11 @@
 const models = require('../models/index');
+const { AuthenticationError } = require('apollo-server-express');
 
 const Query = {
   hello: () => 'Hello world',
   notes: async (parent, args, { models }) => await models.Note.find(),
   note: (parent, args, { models }) => {
-    return models.Note.findById(args.id)
+    return models.Note.findById(args.id);
   },
   user: async (parent, { email }, { models }) => {
     return await models.User.findOne({ email });
@@ -13,18 +14,21 @@ const Query = {
     return await models.User.find({}).limit(100);
   },
   me: async (parent, args, { models, user }) => {
-    console.log(user);
+    if (!user) {
+      return new AuthenticationError('You must be logged in');
+    }
+
     return await models.User.findById(user.id);
   },
-  noteFeed: async (parent, { cursor }, { models }) => {
+  noteFeed: async (parent, { cursor }, { models, user }) => {
     const LIMIT = 10;
     let hasNextPage = false;
     let cursorQuery = {};
 
     if (cursor) {
-      cursorQuery = { _id: { $lt: cursor } }
+      cursorQuery = { _id: { $lt: cursor } };
     }
- 
+
     let notes = await models.Note.find(cursorQuery)
       .sort({ _id: -1 })
       .limit(LIMIT + 1);
@@ -35,13 +39,20 @@ const Query = {
     }
 
     const newCursor = notes[notes.length - 1]._id;
+  
+    notes.forEach((note) => {
+      if (note.favoritedBy.includes(user.id)) {
+        note.set({ favorited: true });
+      }
+
+    });
 
     return {
       notes,
       cursor: newCursor,
       hasNextPage
-    }
-  },
+    };
+  }
 };
 
 module.exports = Query;
